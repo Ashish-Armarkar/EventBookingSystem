366f8018476d979904073e21b12fbf44c6571341
Ashish-Armarkar <ashisharmarkar1999@gmail.com>
2024-10-16 22:20:32 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  baseUrl: "https://ashish-armarkar.github.io/EventBookingSystem"
+  base: "https://ashish-armarkar.github.io/EventBookingSystem"
 })
