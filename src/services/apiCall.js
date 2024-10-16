e0c2b878baa43c56846a7ffb9a2e88c7c9684825
Ashish-Armarkar <ashisharmarkar1999@gmail.com>
2024-10-16 22:35:17 +0530

    base routes updated for public contents

--- src/services/apiCall.js
+++ src/services/apiCall.js
@@ -1,18 +1,18 @@
 export const userData = async () => {
-    const response = await fetch("/users.json")
+    const response = await fetch("/EventBookingSystem/users.json")
     const data = response.json();
     return data;
 
 }
 
 export const eventData = async () => {
-    const response = await fetch("/events.json")
+    const response = await fetch("/EventBookingSystem/events.json")
     const data = response.json();
     return data;
 }
 
 export const eventDetails = async (id) => {
-    const response = await fetch("/events.json")
+    const response = await fetch("/EventBookingSystem/events.json")
     const data = await response.json();
     const eventData = data.find(ele => ele.id == id.substring(1))
 
