a197ca88783d89ff62f5ed4a2859db04c6f52757
Ashish-Armarkar <ashisharmarkar1999@gmail.com>
2024-10-16 22:05:07 +0530

    dist build folder added

---
+++ dist/assets/apiCall-DKqZyrV_.js
@@ -0,0 +1,1 @@
+const o=async()=>(await fetch("/users.json")).json(),c=async()=>(await fetch("/events.json")).json(),r=async s=>(await(await fetch("/events.json")).json()).find(a=>a.id==s.substring(1));export{c as a,r as e,o as u};
